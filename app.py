e9a46cfc75f001d381354d2068d4b9958548aba3
Zach Chaloner <74162569+CodeZ89@users.noreply.github.com>
2022-10-26 13:32:05 -0400

    Nav bar links working

--- app.py
+++ app.py
@@ -16,6 +16,27 @@ def root():
 @app.route('/pets')
 def pets():
     return render_template("pets.j2")
+
+
+@app.route('/adopters')
+def adopters():
+    return render_template("adopters.j2")
+
+@app.route('/fosters')
+def fosters():
+    return render_template("fosters.j2")
+
+@app.route('/shelters')
+def shelters():
+    return render_template("shelters.j2")
+
+@app.route('/foster_shelters')
+def foster_shelters():
+    return render_template("foster_shelters.j2")
+
+@app.route('/adoption_records')
+def adoption_records():
+    return render_template("adoption_records.j2")
 # Listener
 
 
